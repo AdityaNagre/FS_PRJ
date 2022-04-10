a0a523cd57adc6207697b208d2e7773e6b0b1082
Aditya Nagre <81866694+AdityaNagre@users.noreply.github.com>
2022-04-10 17:00:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import NoteState from "./context/Notes/noteStates"
+
+function App() {
+  return (
+    <>
+    <NoteState>
+    <BrowserRouter>
+    <Navbar/>
+    <div className="container">
+    <Routes>
+    <Route exact path="/" element={<Home/>}></Route>
+    <Route exact path="/about" element={<About/>}></Route>
+    </Routes>
+    </div>
+    </BrowserRouter>
+    </NoteState>
+
+    </>
+  );
+}
+
+export default App;
